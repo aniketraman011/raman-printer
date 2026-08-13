543e920e0eae86c7801bcf05d67043537b92f79f
prachiraypadev <prachiraypa.dev@gmail.com>
2026-08-13 16:14:45 +0530

    updated comment

--- raman_printer/printer/app/layout.tsx
+++ raman_printer/printer/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from 'react-hot-toast';
-
+// comment
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
